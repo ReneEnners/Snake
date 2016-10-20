9ce8625520f8543d705d701f7f6e5d5a2b98f599
rene <rene@DESKTOP>
2016-10-20 19:59:18 +0200

    Fenster // Spielfeld // PlayerPosition

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,41 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int playFieldWidth = 140;
+            int playFieldHeight = 40;
+
+            int playerPositionX = 5;
+            int playerPositionY = 5;
+
+            // Initalisierung des Fensters
+            Console.WindowWidth = playFieldWidth + 2;
+            Console.WindowHeight = playFieldHeight + 2;
+            Console.BufferWidth = playFieldWidth + 2;
+            Console.BufferHeight = playFieldHeight + 2;
+            Console.CursorVisible = false;
+
+            // Initialisierung des Spielfeldes
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.BackgroundColor = ConsoleColor.Gray;
+            for (int x = 0; x < playFieldWidth; x++)
+            {
+                for (int y = 0; y < playFieldHeight; y++)
+                {
+                    Console.SetCursorPosition(x + 1, y + 1);
+                    Console.Write(" ");
+                }
+            }
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(playerPositionX, playerPositionY);
+            Console.Write(" ");
+
+            
+
+
+            Console.ReadLine();
         }
     }
 }
